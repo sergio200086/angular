90a4238af99d3e84d206cdeab5fca289d63fa528
sergio200086 <suarezsergio040@gmail.com>
2024-10-01 15:01:39 -0500

    Packaging angular components

--- src/app/counter/components/counter.component.ts
+++ src/app/counter/components/counter.component.ts
@@ -4,9 +4,9 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-counter',
   template: `
     <h3>Counter: {{ counter }}</h3>
-    <button (click)="increaseBy(-1)">-1</button>
-    <button (click)="resetCounter()">Reset</button>
-    <button (click)="increaseBy(1)">+1</button>
+    <button (click)="increaseBy(-1)" class="btn btn-outline-danger">-1</button>
+    <button (click)="resetCounter()" class="btn btn-outline-warning">Reset</button>
+    <button (click)="increaseBy(1)" class="btn btn-outline-primary">+1</button>
   `,
 })
 export class CounterComponent {
